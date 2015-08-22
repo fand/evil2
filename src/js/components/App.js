12b719f1d9f05fd4d571a5635d2982866d2c9b13
fand <fand@gmork.in>
2015-08-22 16:14:51 +0900

    migrate to redux@v1.0.0-alpha

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import EvilApp from './EvilApp';
-import { createRedux } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'redux/react';
 import reducers from '../reducers/index';
 
-const redux = createRedux({root : reducers});
+const store = createStore({root : reducers});
 
 export default class App extends Component {
   render() {
     return (
-      <Provider redux={redux}>
+      <Provider store={store}>
         {() => <EvilApp />}
       </Provider>
     );
