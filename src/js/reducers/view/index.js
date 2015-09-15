9d2378501e834002aac1e4a8b9556ade2bccf4aa
fand <fand@gmork.in>
2015-09-15 09:48:02 +0900

    Move reducer

--- src/js/reducers/view/index.js
+++ src/js/reducers/view/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import clipViewReducer from './clipViewReducer';
-import pianorollReducer from './pianorollReducer';
+import pianorollReducer from '../../pianoroll/reducers/pianorollReducer';
 
 export default function (state, action) {
   const clipView = clipViewReducer(state.clipView, action);
