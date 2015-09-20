dba2acecfeb0ceeb5ca2f8f8d3cfec898422501d
fand <fand@gmork.in>
2015-09-20 20:29:39 +0900

    Make PLAY action to async action

--- src/js/player/CONST.js
+++ src/js/player/CONST.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const DEFAULT_PLAY = {
-  isPlaying : false
+  isPlaying  : false,
+  timer      : null,
+  interval   : 1000,
+  beatLength : 500,
+  bpm        : 120,
+  position   : 0,
 };
 
 const Actions = {
--- src/js/player/actions/PlayerActions.js
+++ src/js/player/actions/PlayerActions.js
@@ -3,6 +3,29 @@
 import { Actions } from '../CONST';
 
 const play = () => {
+
+  return (dispatch, getState) => {
+
+    const interval = getState().playerData.interval;
+
+    const tick = () => {
+      const state = getState();
+      if (!state.playerData.isPlaying) { return; }
+
+      dispatch({
+        ...state,
+        playerData : {
+          ...state.playerData,
+          position : position + (interval / beatLength),
+        },
+      });
+
+      setTimeout(tick, interval);
+    };
+
+    tick();
+  };
+
   return {
     type : Actions.PLAY,
   };
