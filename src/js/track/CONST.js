81a2580d7867071e0c15eec6b262d380951e7ece
fand <fand@gmork.in>
2015-09-21 20:14:47 +0900

    Add DEFAULT_TRACK prototype

---
+++ src/js/track/CONST.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import uuid from 'uuid';
+
+const DEFAULT_TRACK = {
+  entities : {
+    track : {
+      0 : {
+        uuid     : 0,
+        name     : 'Drum',
+        clip     : null,
+        deviceId : 0,
+      },
+      1 : {
+        uuid     : 1,
+        name     : 'Bass',
+        clip     : null,
+        deviceId : 1,
+      },
+    },
+  },
+  tracks : [0, 1],
+};
+
+const Actions = {
+  TICK     : Symbol(),
+  SET_CLIP : Symbol(),
+};
+
+export default {
+  Actions,
+  DEFAULT_TRACK,
+};
