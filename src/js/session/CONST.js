b5df935050b6a7058fdfcc71f88b93d951ee24f7
fand <fand@gmork.in>
2015-09-26 00:17:28 +0900

    Changed session state structure / Move session action names to CONST file

--- src/js/session/CONST.js
+++ src/js/session/CONST.js
@@ -2,27 +2,39 @@
 
 import uuid from 'uuid';
 
+const Actions = {
+  SELECT_CLIP  : 'SELECT_CLIP',
+  SELECT_SCENE : 'SELECT_SCENE',
+  SELECT_CELL  : 'SELECT_CELL',
+};
+
 const DEFAULT_SESSION = {
     scenes : [],
 };
 
+const uuids = [uuid.v4(), uuid.v4()];
 const DEMO_SESSION = {
-  scenes : [{
-    uuid        : uuid.v4(),
-    name        : 'scene1',
-    clipIds     : ['aaa', 'bbb'],
-    controls    : [],
-    beatsPerBar : 4
-  }, {
-    uuid        : uuid.v4(),
-    name        : 'scene2',
-    clipIds     : ['ccc', 'ddd'],
-    controls    : [],
-    beatsPerBar : 4
-  }],
+  entities: {
+    [uuids[0]] : {
+      uuid        : uuids[0],
+      name        : 'scene1',
+      clipIds     : ['aaa', 'bbb'],
+      controls    : [],
+      beatsPerBar : 4,
+    },
+    [uuids[1]] : {
+      uuid        : uuids[1],
+      name        : 'scene2',
+      clipIds     : ['ccc', 'ddd'],
+      controls    : [],
+      beatsPerBar : 4,
+    },
+  },
+  scenes : uuids,
 };
 
 export default {
+  Actions,
   DEFAULT_SESSION,
   DEMO_SESSION,
 };
--- src/js/session/actions/SessionActions.js
+++ src/js/session/actions/SessionActions.js
@@ -1,28 +1,24 @@
 'use strict';
 
-// import { SELECT_CLIP } from '../CONST';
-
-const SELECT_CLIP  = 'SELECT_CLIP';
-const SELECT_SCENE = 'SELECT_SCENE';
-const SELECT_CELL  = 'SELECT_CELL';
+import { Actions } from '../CONST';
 
 export function selectClip (clipId) {
   return {
-    type   : SELECT_CLIP,
+    type   : Actions.SELECT_CLIP,
     clipId,
   };
 }
 
 export function selectCell (pos) {
   return {
-    type : SELECT_CELL,
+    type : Actions.SELECT_CELL,
     pos,
   };
 }
 
 export function selectScene (index) {
   return {
-    type : SELECT_SCENE,
+    type : Actions.SELECT_SCENE,
     index,
   };
 }
